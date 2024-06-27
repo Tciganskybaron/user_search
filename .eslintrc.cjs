dc624c806c2c432026c99ef51e5e3a37de1dd70d
Nikita <nikita@MBP-Macbook.home>
2024-06-27 09:41:34 +0200

    prettier + eslint, plugin:prettier/recommended

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,31 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier'],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
-	settings: {
-		'import/resolver': {
-			typescript: {
-				alwaysTryTypes: true,
-			},
-			node: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-		},
-	},
-	rules: {
-		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-	},
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended', // Включение плагина Prettier
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'error', // Обработка ошибок Prettier как ошибок ESLint
+  },
 };
