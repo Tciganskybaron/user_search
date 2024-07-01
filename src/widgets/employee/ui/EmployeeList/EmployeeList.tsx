1ebb87e34eed25b689eb3c5c4bb54421589d5779
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-07-01 15:13:12 +0200

    loading logic

--- src/widgets/employee/ui/EmployeeList/EmployeeList.tsx
+++ src/widgets/employee/ui/EmployeeList/EmployeeList.tsx
@@ -1,23 +1,33 @@
 import { SmallCardEmployee } from '~features/employee/ui';
+import classNames from 'classnames';
 import styles from './EmployeeList.module.scss';
 import { useEmployeeStore } from '~features/employee/model/useEmployeeStore';
 import { EmployeeListProps } from './EmployeeList.props';
+import { Preloader } from '~shared/ui';
 
 export const EmployeeList = ({ ...props }: EmployeeListProps) => {
-  const { notFound, employees, employee } = useEmployeeStore();
+  const { loading, notFound, employees, employee } = useEmployeeStore();
 
   return (
-    <ul className={styles['search-box-ul']} {...props}>
-      {employees.map((user) => (
-        <SmallCardEmployee
-          key={user.username}
-          employee={user}
-          selected={user.email === employee?.email}
-        />
-      ))}
+    <ul
+      className={classNames(styles['search-box-ul'], {
+        [styles.loading]: loading,
+      })}
+      {...props}
+    >
+      {employees &&
+        !loading &&
+        employees.map((user) => (
+          <SmallCardEmployee
+            key={user.username}
+            employee={user}
+            selected={user.email === employee?.email}
+          />
+        ))}
       {notFound && (
         <li className={styles['search-not-found']}>ничего не найдено </li>
       )}
+      {loading && <Preloader />}
     </ul>
   );
 };
